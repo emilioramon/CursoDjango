c321d5855a56e5963e955d747e96f5d0c09529c4
Emilio Ramon <emilioramon@gmail.com>
2022-03-25 20:28:30 -0300

    Third commit

--- Projecto1/Projecto1/views.py
+++ Projecto1/Projecto1/views.py
@@ -2,12 +2,19 @@ import datetime
 from django.http import HttpResponse
 from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
 def saludo(request): # primera vista
 
+    p1=Persona("Emilio H","Ramon")
+    ahora=fecha_actual=datetime.datetime.now()
     doc_externo=open("C:/Users/soporte/Documents/Proyectos Django/Projecto1/Projecto1/Platillas/Hola.html")
     plt=Template(doc_externo.read())
     doc_externo.close()
-    ctx=Context()
+    ctx=Context({"nombre_persona":p1.nombre,"apellido_persona":p1.apellido,"momento_actual":ahora})
     documento=plt.render(ctx)
     
     return HttpResponse(documento)
